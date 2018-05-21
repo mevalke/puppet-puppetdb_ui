f3733389c9310a4668dda80c7e7a8dd93bf57354
Miikka Valkeapaa <miikka1973@gmail.com>
2018-05-21 19:45:42 +0300

    fixes the postgres_host variable definition in puppetdb_pm.pp and tidies up the code

--- manifests/puppetdb_pm.pp
+++ manifests/puppetdb_pm.pp
@@ -1,5 +1,5 @@
 class puppetdb_ui::puppetdb_pm {
-  $pdb_postgres_host = lookup(pdb_postgres_host)
+  $pdb_postgres_host = $puppetdb_ui::pdb_postgres_host
   class {'puppetdb::master::config':
     puppetdb_server         => $pdb_postgres_host,
     manage_report_processor => true,
