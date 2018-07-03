aba4b4d68487b30f00531a21505c3119a6c9db9d
miikka.valkeapaa <miikka1973@gmail.com>
2018-07-03 18:55:29 +0300

    adds apt cache update and sources.list moodification

--- manifests/puppetdb_setup.pp
+++ manifests/puppetdb_setup.pp
@@ -1,10 +1,16 @@
 class puppetdb_ui::puppetdb_setup {
+ exec { disable_apt_cdrom_source:
+	  command => "sed -i '/cdrom/s/^/#/g' /etc/apt/sources.list",
+	  onlyif  => "grep -E '^deb cdrom' /etc/apt/sources.list",
+	  path    => '/bin:/usr/bin:/usr/local/bin',
+  } ->
+  class { 'apt': } ->
   package {'apt-transport-https':
     ensure => present
-  }
+  } ->
   class {'puppetdb::server':
     database_host => '127.0.0.1',
-  }
+  } ->
   class {'puppetdb::database::postgresql':
     listen_addresses 	=> '127.0.0.1',
   }
