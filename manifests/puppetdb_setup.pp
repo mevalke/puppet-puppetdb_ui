880ca86b3ebd58cf341d96222f85fa3131bb395c
Miikka Valkeapaa <miikka1973@gmail.com>
2018-07-05 19:41:39 +0300

    fixes line indentations and adds absolute path to sed

--- manifests/puppetdb_setup.pp
+++ manifests/puppetdb_setup.pp
@@ -1,12 +1,12 @@
 class puppetdb_ui::puppetdb_setup {
   exec { disable_apt_cdrom_source:
-	  command => "sed -i '/cdrom/s/^/#/g' /etc/apt/sources.list",
-	  onlyif  => "grep -E '^deb cdrom' /etc/apt/sources.list",
-	  path    => '/bin:/usr/bin:/usr/local/bin',
+    command => "/bin/sed -i '/cdrom/s/^/#/g' /etc/apt/sources.list",
+    onlyif  => "grep -E '^deb cdrom' /etc/apt/sources.list",
+    path    => '/bin:/usr/bin:/usr/local/bin',
   } ->
   exec { update_apt_cache:
-          command => "/usr/bin/apt-get update",
-          path    => '/bin:/usr/bin:/usr/local/bin',
+    command => "/usr/bin/apt-get update",
+    path    => '/bin:/usr/bin:/usr/local/bin',
   } ->
   package {'apt-transport-https':
     ensure => present
